228e5ab79dda675d8d86efee9f04617fe0fe6814
sacanlee <sacanlee97@gmail.com>
2023-07-02 15:01:29 +0330

    model name correction and beautification

--- web/models.py
+++ web/models.py
@@ -7,9 +7,13 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    def __str__(self):
+        return f"{self.text}:   Amount: {self.amount}    Date: {self.date}"
+    
 class Income(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.text}:   Amount: {self.amount}    Date: {self.date}"

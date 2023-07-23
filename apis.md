a0c28eb9e557cc32f1df2704b2a70b2689aa78f1
root <root@LAPTOP-SINA>
2023-07-23 16:05:18 +0330

    back again to ptogect anfter crash

---
+++ apis.md
@@ -0,0 +1,11 @@
+/submit/expense/
+POST , return querydict
+input date(Optinal), text, amount, user
+output status : ok
+
+/submit/income/
+POST , return querydict
+input date(Optinal), text, amount, user
+output status : ok
+
+post get by get method
--- web/urls.py
+++ web/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     re_path(r'^submit/expense/$', views.submit_expense, name='submit_expense'),
+    re_path(r'^submit/income/$', views.submit_expense, name='submit_income'),
+
     # add any other URL patterns here
 ]

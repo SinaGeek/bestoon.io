f848e26d09198ca9d793ca31cc158088e32a5bd2
root <root@LAPTOP-SINA>
2023-07-27 19:30:25 +0330

    User regiteretion add to project

--- web/urls.py
+++ web/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     re_path(r'^submit/expense/$', views.submit_expense, name='submit_expense'),
-    re_path(r'^submit/income/$', views.submit_expense, name='submit_income'),
+    re_path(r'^submit/income/$', views.submit_income, name='submit_income'),
+    re_path(r'^accounts/register/$', views.register, name='register'),
+
+    re_path(r'^$', views.index, name='index'),
 
     # add any other URL patterns here
 ]

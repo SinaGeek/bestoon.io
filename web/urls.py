ae2145f85ac5893c3d8bab73a7dfb1a24fd8c126
root <root@LAPTOP-SINA>
2023-09-07 17:54:45 +0330

    resolve logout issue

--- web/urls.py
+++ web/urls.py
@@ -1,10 +1,12 @@
 from django.urls import re_path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     re_path(r'^submit/expense/$', views.submit_expense, name='submit_expense'),
     re_path(r'^submit/income/$', views.submit_income, name='submit_income'),
     re_path(r'^accounts/register/$', views.register, name='register'),
+    re_path(r'^accounts/logout/$', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
     re_path(r'^$', views.index, name='index'),
 
     # add any other URL patterns here
